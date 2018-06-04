3f2783d8e87a3a168bb8c9a25e04fbb8ece1e8ce
NingAnMe <ninganme@qq.com>
2018-06-04 18:16:38 +0800

    修改程序：删除头部无用的文件

--- cross_c04_time_series_bias.py
+++ cross_c04_time_series_bias.py
@@ -8,18 +8,13 @@ Created on 2018年4月20日
 """
 import os
 import sys
-import calendar
 from datetime import datetime
-from multiprocessing import Pool, Lock
 
 import numpy as np
 from matplotlib.ticker import MultipleLocator
 
 from configobj import ConfigObj
 from dateutil.relativedelta import relativedelta
-from numpy.lib.polynomial import polyfit
-from numpy.ma.core import std, mean
-from numpy.ma.extras import corrcoef
 
 from PB.CSC.pb_csc_console import LogServer
 from PB import pb_time, pb_io
@@ -30,9 +25,7 @@ from DV.dv_pub_3d import bias_information, day_data_write, get_bias_data, get_ca
 from DV.dv_pub_3d import FONT0, FONT_MONO, FONT1
 from DM.SNO.dm_sno_cross_calc_map import RED, BLUE, EDGE_GRAY, ORG_NAME, mpatches
 
-from PB.pb_time import is_day_timestamp_and_lon
-
-from cross_pb_io import ReadHDF5, loadYamlCfg
+from cross_pb_io import loadYamlCfg
 
 
 def run(pair, date_s, date_e):
@@ -972,10 +965,6 @@ if __name__ == "__main__":
     ABC_DIR = GLOBAL_CONFIG['PATH']['OUT']['ABC']
     LogPath = GLOBAL_CONFIG['PATH']['OUT']['LOG']
     Log = LogServer(LogPath)
-
-    # 开启进程池
-    threadNum = GLOBAL_CONFIG['CROND']['threads']
-    pool = Pool(processes=int(threadNum))
 
     if len(ARGS) == 2:
         satPair = ARGS[0]
